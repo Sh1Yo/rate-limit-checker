0899e61e231b175bb496c6a76f8ba092224de7a8
Alexander Mironov <sh1yo@tuta.io>
2020-05-11 10:00:19 +0400

    Initial commit

---
+++ rate.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var threads int
+var method string
+var link string
+var code int
+var cont bool
+
+func main() {
+	var wg sync.WaitGroup
+	var channel = make(chan int)
+	var times int
+	flag.StringVar(&method, "X", "HEAD", "method")
+	flag.IntVar(&threads, "t", 10, "threads")
+	flag.IntVar(&times, "a", 500, "times")
+	flag.BoolVar(&cont, "s", false, "continue after the code changing")
+	flag.StringVar(&link, "u", "", "link")
+	flag.Parse()
+
+	req, _ := http.NewRequest(method, link, nil) //,nil
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
+	resp, _ := (*http.Client).Do(&http.Client{}, req)
+	code = resp.StatusCode
+
+	for t := 0; t < threads; t++ {
+		go func() {
+			for {
+				i := <-channel
+				wg.Add(1)
+				request(i)
+				wg.Done()
+			}
+		}()
+	}
+	for i := 0; i < times; i++ {
+		channel <- i
+	}
+	wg.Wait()
+}
+
+func request(i int) {
+	client := &http.Client{}
+	req, _ := http.NewRequest(method, link, nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		println(err.Error())
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		println(err.Error())
+	}
+	fmt.Println(i, resp.StatusCode, len(body))
+	if code != resp.StatusCode && !cont {
+		os.Exit(5)
+	}
+}
